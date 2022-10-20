009e635c32b530e2c412359524a00fde53dc2f43
aleksseii <alesha.shkel@mail.ru>
2022-10-21 00:40:47 +0300

    created project and added .gitignore file

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+allprojects {
+    group = "ru.aleksseii"
+    version = "1.0-SNAPSHOT"
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
